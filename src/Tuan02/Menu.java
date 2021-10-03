182dc3c1d748fe531949af8fb42b7e5fce6aa389
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2021-10-03 20:20:44 +0700

    đẩy

--- src/Tuan02/Menu.java
+++ src/Tuan02/Menu.java
@@ -6,87 +6,87 @@ public class Menu {
     public static void main(String[] args) 
     {       
         System.out.println("--------- MEUNU ---------");
-        System.out.println("1/ GIAI PHUONG TRINH BAC NHAT ");
-        System.out.println("2/ GIAI PHUONG TRINH BAC HAI ");
-        System.out.println("3/ TINH TIEN DIEN ");
-        System.out.println("-----------------------------------------");
-        Scanner nhapChucNang = new Scanner(System.in);
-        System.out.println("Nhap Vao Chuc Nang Can Thuc Hien");
-        int chucNang = nhapChucNang.nextInt();
-        switch(chucNang)
+        System.out.println("1/ Giải PT Bậc 1 ");
+        System.out.println("2/ Giải PT Bậc 2 ");
+        System.out.println("3/ Tính Tiền Điện ");
+        System.out.println("4/ Tính ChuViDienTich HCN ");
+        System.out.println("--------------------------");
+        Scanner NhapChucNang = new Scanner(System.in);
+        System.out.println("Nhập chức năng muốn sử dụng");
+        int ChucNang = NhapChucNang.nextInt();
+        switch(ChucNang)
         {
             case 1:
             {                   
-                double nhapvaoa, nhapvaob;
-                System.out.println("Nhap Vao a: ");
-                nhapvaoa = nhapChucNang.nextDouble();
-                System.out.println("Nhap Vao b: ");
-                nhapvaob = nhapChucNang.nextDouble();
-                giaiPT1(nhapvaoa, nhapvaob);
+                double NhapVaoA, NhapVaoB;
+                System.out.println("Nhập a: ");
+                NhapVaoA = NhapChucNang.nextDouble();
+                System.out.println("Nhập b: ");
+                NhapVaoA = NhapChucNang.nextDouble();
+                PTBac1(NhapVaoA, NhapVaoA);
             }; break;
 
             case 2:
             {
-                double soA, soB, soC;
-                System.out.println("Nhap Vao a: ");
-                soA = nhapChucNang.nextDouble();
-                System.out.println("Nhap Vao b: ");
-                soB = nhapChucNang.nextDouble();
-                System.out.println("Nhap Vao c: ");
-                soC = nhapChucNang.nextDouble();
-                giaiPT2(soA, soB, soC);             
+                double SoA, SoB, SoC;
+                System.out.println("Nhập a: ");
+                SoA = NhapChucNang.nextDouble();
+                System.out.println("Nhập b: ");
+                SoB = NhapChucNang.nextDouble();
+                System.out.println("Nhập c: ");
+                SoC = NhapChucNang.nextDouble();
+                PTBac2(SoA, SoB, SoC);             
             }; break;
 
             case 3:
             {
-                double nhapsuDungDien;
-                System.out.println("Nhap Vao So Dien Ma Ban Da Su Dung: ");
-                nhapsuDungDien = nhapChucNang.nextDouble();
-                tinhTiendien(nhapsuDungDien);               
+                double DienSuDung;
+                System.out.println("Nhập số điện: ");
+                DienSuDung = NhapChucNang.nextDouble();
+                TinhTiendien(DienSuDung);               
             }; break;
 
-            default: System.out.println("Nhap Sai ");
+            default: System.out.println("Nhập Sai ");
         }
 
     }
-    static void giaiPT1(double nhapVaoSoA, double nhapVaoSoB)
+    static void PTBac1(double a, double b)
     {
-        if(nhapVaoSoA ==0)
-            if(nhapVaoSoB ==0)
-                System.out.println("Vo So Nghiem");
+        if(a==0)
+            if(b==0)
+                System.out.println("Vô số Nghiệm");
             else
-                System.out.println("Vo Nghiem");
+                System.out.println("Vô Nghiệm");
         else
         {
-            double xc = - nhapVaoSoB / nhapVaoSoA;
-            System.out.println("x = "+ xc);
-        }      
+            double x = - b/ a;
+            System.out.println("x = "+ x);
+        } 
     } 
 
-    static void giaiPT2(double nhapVaoSoa, double nhapVaoSob, double nhapVaoSoc)
+    static void PTBac2(double SoA, double SoB, double SoC)
     {
-        if(nhapVaoSoa ==0)
-            if(nhapVaoSob ==0)
-                if(nhapVaoSoc ==0)
-                    System.out.println("Vo So Nghiem");
+        if(SoA==0)
+            if(SoB ==0)
+                if(SoC ==0)
+                    System.out.println("Vô số nghiệm");
                 else
-                    System.out.println("Phuong Trinh Vo Nghiem");
+                    System.out.println("Vô Nghiệm");
             else
             {
-                double x = -nhapVaoSoc / nhapVaoSob;
+                double x = -SoC / SoC;
                 System.out.println("X = "+ x);
             }
         else   
             {
                 double delta, x1, x2;
-                delta = nhapVaoSob * nhapVaoSob - 4 * nhapVaoSoa * nhapVaoSoc;
+                delta = SoB * SoB - 4 * SoA * SoC;
 
                 if(delta ==0)
                 {
                     System.out.println("Delta = "+ delta);
-                    System.out.printf("x1 = x2 = %f \n", - nhapVaoSob / 2 *nhapVaoSoa);
+                    System.out.printf("x1 = x2 = %f \n", - SoB / 2 *SoA);
                 }
-
                 if(delta <0)
                 {
                     System.out.println("Delta = "+ delta);
@@ -95,17 +95,16 @@ public class Menu {
 
                 if(delta >0)
                 {
-                    x1 = -nhapVaoSob + Math.sqrt(delta) / 2 * nhapVaoSoa;
-                    x2 = -nhapVaoSob - Math.sqrt(delta) / 2 * nhapVaoSoa;
+                    x1 = -SoB + Math.sqrt(delta) / 2 * SoA;
+                    x2 = -SoB- Math.sqrt(delta) / 2 * SoA;
                     System.out.println("Delta = "+ delta);
                     System.out.println("x1 = "+ x1);
                     System.out.println("x2 = "+ x2);
-                }                   
+                }
             }
+        }
 
-    }
-
-    static void tinhTiendien(double nhapsodienSuDung)
+    static void TinhTiendien(double nhapsodienSuDung)
     {
         if(nhapsodienSuDung < 50 )
             System.out.println("So Tien La: "+ nhapsodienSuDung * 1000);
