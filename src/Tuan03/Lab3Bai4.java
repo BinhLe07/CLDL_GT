c193a52ba85c0e6b9992ce1a60780f8feae7b091
LeDoQuocBinh_2008110078_K14DCPM01 <binhle030601@gmail.com>
2021-10-11 07:20:14 +0700

    đẩy

--- src/Tuan03/Lab3Bai4.java
+++ src/Tuan03/Lab3Bai4.java
@@ -7,41 +7,41 @@ public class Lab3Bai4 {
         Scanner DuLieu = new Scanner(System.in);
         System.out.print("Nhập số sinh viên: ");
         int n = DuLieu.nextInt();
-        String ho_Ten[] = new String[n];
-        double diem[] = new double[n];
+        String HoTen[] = new String[n];
+        double Diem[] = new double[n];
         for(int i = 0;i < n;i++) {
             DuLieu.nextLine();
             System.out.printf("Họ và tên sinh viên thứ %d: ", i+1);
-            ho_Ten[i] = DuLieu.nextLine();
+            HoTen[i] = DuLieu.nextLine();
             System.out.printf("Điểm sinh viên thứ %d: ", i+1);
-            diem[i] = DuLieu.nextDouble();
+            Diem[i] = DuLieu.nextDouble();
         }
         for(int i = 0;i < n;i++){
             for(int j = i + 1;j < n - 1;j++){
-                if(diem[i] > diem[j]){
-                    double tam = diem[i];
-                    diem[i] = diem[j];
-                    diem[j] = tam;
-                    String tam2 = ho_Ten[i];
-                    ho_Ten[i] = ho_Ten[j];
-                    ho_Ten[j] = tam2;
+                if(Diem[i] > Diem[j]){
+                    double tam = Diem[i];
+                    Diem[i] = Diem[j];
+                    Diem[j] = tam;
+                    String tam2 = HoTen[i];
+                    HoTen[i] = HoTen[j];
+                    HoTen[j] = tam2;
                 }
             }
         }
         System.out.printf("Danh sách sinh viên");
         for(int i = 0;i < n;i++){
-            System.out.printf("\nHọ và tên: %s\n", ho_Ten[i]);
-            System.out.printf("Điểm: %.2f\n", diem[i]);
-            if(diem[i] >= 9)
+            System.out.printf("\nHọ và tên: %s\n", HoTen[i]);
+            System.out.printf("Điểm: %.2f\n", Diem[i]);
+            if(Diem[i] >= 9)
                 System.out.println("Xếp loại: Xuất sắc");
             else{
-                if(diem[i] >= 7.5)
+                if(Diem[i] >= 7.5)
                     System.out.println("Xếp loại: Giỏi");
                 else{
-                    if(diem[i] >= 6.5)
+                    if(Diem[i] >= 6.5)
                         System.out.println("Xếp loại: Khá");
                     else{
-                        if(diem[i] >= 5)
+                        if(Diem[i] >= 5)
                             System.out.println("Xếp loại: Trung bình");
                         else    
                             System.out.println("Xếp loại: Yếu");
